fecff4f01a057d3975b3f8af1d4104e8d3cfa4fd
Bryan-Feighner <bfeighne@stevens.edu>
2023-08-08 21:32:09 -0400

    Updated RPS.py (Not working)

--- RPS.py
+++ RPS.py
@@ -6,19 +6,26 @@ on = 1
 gameCount = 0
 winCount = 0
 winRate = 0.0
-while on == 1:
+def chooseNum():
+    global playerChoice
+    global computerChoice
     print("Please type rock, paper, or scissors to select your move!")
     input(playerChoice)
-    random = random.randint(0,2)
-    if random == 2:
+    randomNum = random.randint(0,2)
+    if randomNum == 2:
         computerChoice = 'rock'
         print('The computer has selected rock!')
-    elif random == 1:
+    elif randomNum == 1:
         computerChoice = 'paper'
         print('The computer has selected paper!')
-    elif random == 0:
+    else:
         computerChoice = 'scissors'
         print('The computer has selected scissors!')
+def gameState():
+    global winCount
+    global gameCount
+    global playerChoice
+    global computerChoice
     if playerChoice == computerChoice:
         print('You have tied! Nobody wins!\n')
         gameCount+=1
@@ -43,8 +50,11 @@ while on == 1:
     elif playerChoice == 'paper' and computerChoice == 'scissors':
         print('You have lost!\n')
         gameCount+=1
+while on == 1:
+    chooseNum()
+    gameState()
     #winRate = winCount/gameCount
     print("You've won " + str(winCount) + " out of " + str(gameCount) + " games!\n")
-    print('Your current winrate is ' + str(winRate) + "\n")
+    #print('Your current winrate is ' + str(winRate) + "\n")
     print("Would you like to play again? Type 1 for yes and 0 for no!\n")
     input(on)
